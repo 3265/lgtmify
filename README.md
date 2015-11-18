ff9dd5c8e31bc973542e4cdd7a1a81a55d4e198b
mgoldchild <mgoldchild@gmail.com>
2015-11-19 03:38:28 +0900

    Add test and jsdoc

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# lgtmify
+# lgtmify  
+
+##Overview  
+A LGTM generator write in node. Implemented on node-opencv and node-gm modules.
+
+##Install  
+You'll need OpenCV and Graphicsmagick before install LGTMify.
+```
+$ npm install -g lgtmify
+```
+
+##Usage  
+```
+$ lgtmify /your/image path
+```
+
+##Contributers  
+
+##License  
+MIT
+
--- test/lib/lgtmify.js
+++ test/lib/lgtmify.js
@@ -110,9 +110,38 @@ describe('Lgtmify', function () {
     });
     it('ok', function (done) {
       lgtmify.setImageInfo(function (error) {
-        assert.ok( !error );
+        assert.ok(!error);
         assert.strictEqual(lgtmify.xl, 2);
         assert.strictEqual(lgtmify.yl, 1);
+        done();
+      });
+    });
+  });
+
+  describe('getFaceSquaresInfo', function () {
+    before(function (done) {
+      lgtmify = new Lgtmify();
+      lgtmify.setImage({
+        detectObject: function (a, b, cb) {
+          return cb(null, [
+            {x:1,y:1,width:10,height:20},
+            {x:1,y:1,width:10,height:20}
+          ]);
+        }
+      });
+      lgtmify.setOpencv({
+        FACE_CASCADE: 1
+      });
+      done();
+    });
+    it('ok', function (done) {
+      var expect = [
+        { minx: 1, maxx: 11, miny: 1, maxy: 21 },
+        { minx: 1, maxx: 11, miny: 1, maxy: 21 }
+      ];
+      lgtmify.getFaceSquaresInfo(function (error, faceSquaresInfo) {
+        assert.ok(!error);
+        assert.deepEqual(faceSquaresInfo, expect);
         done();
       });
     });
@@ -160,6 +189,59 @@ describe('Lgtmify', function () {
       };
       var result = lgtmify.getLargestRectangularArea(rowNo, histogram );
       assert.deepEqual( result, expect );
+      done();
+    });
+  });
+
+  describe('getLGTMInfo', function () {
+    it('ok portrait', function (done) {
+      lgtmify = new Lgtmify();
+      var lgtm = {
+        width: 100,
+        height: 300,
+        mix: 0,
+        miny: 0,
+        maxx: 30,
+        maxy: 50,
+        maxv: 100
+      };
+      var expect = { width: 100, height: 30, offsetx: 0, offsety: -85, fontSize: 35 };
+      var result = lgtmify.getLGTMInfo(lgtm);
+      assert.deepEqual(result, expect);
+      done();
+    });
+    it('ok landscape', function (done) {
+      lgtmify = new Lgtmify();
+      var lgtm = {
+        width: 300,
+        height: 100,
+        mix: 0,
+        miny: 0,
+        maxx: 30,
+        maxy: 50,
+        maxv: 100
+      };
+      var expect = { width: 300, height: 91, offsetx: 0, offsety: 45, fontSize: 107 };
+      var result = lgtmify.getLGTMInfo(lgtm);
+      assert.deepEqual(result, expect);
+      done();
+    });
+    it('no face', function (done) {
+      lgtmify = new Lgtmify();
+      var lgtm = {
+        width: 0,
+        height: 0,
+        mix: 10,
+        miny: 10,
+        maxx: 20,
+        maxy: 50,
+        maxv: 0
+      };
+      lgtmify.xl = 100;
+      lgtmify.yl = 100;
+      var expect = { width: 100, height: 30, offsetx: 0, offsety: 65, fontSize: 35 };
+      var result = lgtmify.getLGTMInfo(lgtm);
+      assert.deepEqual(result, expect);
       done();
     });
   });
